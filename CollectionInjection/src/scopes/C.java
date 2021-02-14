13a42cf7c278e738617f25212f129cc805159090
Prashant <70711348+Prince016@users.noreply.github.com>
2021-02-14 21:22:45 +0530

    list map set

---
+++ CollectionInjection/src/scopes/C.java
@@ -0,0 +1,20 @@
+package scopes;
+
+public class C
+{
+	
+	
+	
+	
+	
+	public C() {
+		// TODO Auto-generated constructor stub
+	
+		System.out.println("constructor from the Class C");
+	}
+
+
+
+	
+
+}
