f47f8f0c1bc61b3a9c9cecb551302afee2526413
Prashant <70711348+Prince016@users.noreply.github.com>
2021-02-14 21:28:18 +0530

    Scopes

---
+++ Scopes/src/scopes/A.java
@@ -0,0 +1,21 @@
+package scopes;
+
+public class A {
+	
+	public A() {
+		// TODO Auto-generated constructor stub
+	System.out.println("constructor from the Class A ");
+	}
+	private B bclass;
+	
+	
+	public B getBclass() {
+		return bclass;
+	}
+	public void setBclass(B bclass) {
+		this.bclass = bclass;
+	}
+	
+	
+	
+}
---
+++ Scopes/src/scopes/B.java
@@ -0,0 +1,24 @@
+package scopes;
+
+public class B {
+
+	private C cclass;
+
+	public B() {
+		// TODO Auto-generated constructor stub
+	System.out.println("constructor from the Class B ");
+	}
+	
+	public C getCclass() {
+		return cclass;
+		
+	}
+
+	public void setCclass(C cclass) {
+		this.cclass = cclass;
+	}
+
+	
+	
+	
+}
