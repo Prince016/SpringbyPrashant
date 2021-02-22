c1e2605afc0464ea29495183857e01e6258f80c4
Prashant <70711348+Prince016@users.noreply.github.com>
2021-02-22 12:41:38 +0530

    update address

--- AnnotationExercise/src/test/java/javaEmploye/Address.java
+++ AnnotationExercise/src/test/java/javaEmploye/Address.java
@@ -1,5 +1,26 @@
 package javaEmploye;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Address {
 
+	private String location;
+
+	
+	
+	
+	
+	public Address(String location) {
+		
+		this.location = location;
+		System.out.println("location is :-> "+location);
+	}
+
+	
+	
+	
+	
+	
 }
+
