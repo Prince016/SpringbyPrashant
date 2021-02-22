d692b3d532261f924ac79c21220660596f2f06b6
Prashant <70711348+Prince016@users.noreply.github.com>
2021-02-22 12:43:39 +0530

    update config file

--- AnnotationExercise/src/test/java/javaEmploye/ConfigApp.java
+++ AnnotationExercise/src/test/java/javaEmploye/ConfigApp.java
@@ -6,19 +6,21 @@ import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
-@ComponentScan("javaEmploye")
+//@ComponentScan("javaEmploye")
 public class ConfigApp {
 
 	
 	@Bean
 	public Address addobj() {
-		return new Address();
+		return new Address("delhi");
 	}
 	
 	
 	
 	@Bean
 	public Employee empobj() {
+		
+		
 		return new Employee(addobj());
 	}
 	
